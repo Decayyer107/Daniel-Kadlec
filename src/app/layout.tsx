fcefedac8d0c56555484abc4f5cead3bf40fe518
Decayyer <daniel-kadlec@seznam.cz>
2025-09-18 12:36:56 +0200

    refactor(lang button): move to new component and update switch style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import ThemeProvider from "@/components/ThemeProvider";
 import { LanguageProvider } from "@/utils/LanguageContext";
-import LanguageSwitcher from "../components/LanguageSwicther";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
@@ -10,7 +9,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <body className="transition-colors duration-500 antialiased">
         <ThemeProvider>
             <LanguageProvider>
-                <LanguageSwitcher />
                 <Navbar />
                 {children}
             </LanguageProvider>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useEffect, useState } from "react";
+import LanguageSwitcher from "../components/LanguageSwicther";
 
 export default function Navbar() {
     const [theme, setTheme] = useState<"light" | "dark" >("dark");
@@ -25,19 +26,21 @@ export default function Navbar() {
     if (!theme) return null;
 
     return (
-        <nav className="w-full h-16 flex items-center justify-between px-4 transition-colors duration-500 bg-gray-300 dark:bg-gray-950">
+        <nav className="w-full h-16 flex items-center justify-between px-4 transition-colors duration-500 bg-gray-300 dark:bg-gray-950 absolute top-0">
             <ul className="flex gap-4">
                 <li><a className="text-black dark:text-white" href="/">Třeba Domů</a></li>
                 <li><a className="text-black dark:text-white" href="#">Třeba Kontakt</a></li>
                 <li><a className="text-black dark:text-white" href="#">Třeba o mě</a></li>
             </ul>
-
-            <button
-                className="p-2 bg-gray-200 dark:bg-gray-800 text-black dark:text-white rounded-md transition-colors duration-300"
-                onClick={toggleTheme}
-            >
-                {theme === "dark" ? "Light" : "Dark"}
-            </button>
+            <div className={'flex gap-4'}>
+                <LanguageSwitcher/>
+                <button
+                    className="p-2 bg-gray-200 dark:bg-gray-800 text-black dark:text-white rounded-md transition-colors duration-300"
+                    onClick={toggleTheme}
+                >
+                    {theme === "dark" ? "Light" : "Dark"}
+                </button>
+            </div>
         </nav>
     );
 }
