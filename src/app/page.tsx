30f3132ff44abf0afa923bb9a8339b520b54751b
daniel.kadlec <daniel-kadlec@seznam.cz>
2025-10-01 10:45:13 +0200

    feat: fonts working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
 
   return (
       <div className="min-h-screen flex flex-col items-center justify-center bg-white dark:bg-black text-black dark:text-white transition-colors duration-500">
-      <h1 className="text-4xl font-bold mb-4">{dict.common.hello}</h1>
-      <p className="text-lg text-black dark:text-gray-300">{dict.common.paragraph}</p>
+      <h1 className="text-4xl font-primary mb-4">{dict.common.hello}</h1>
+      <p className="text-lg font-secondary text-black dark:text-gray-300">{dict.common.paragraph}</p>
       </div>
   );
 }
