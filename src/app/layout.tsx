6a54c9efe21753acd73a7de21a5da497e4fee99a
Decayyer <daniel-kadlec@seznam.cz>
2025-10-02 15:10:03 +0200

    fix: body background flicker on load

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,13 @@ import { LanguageProvider } from "@/utils/LanguageContext";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-        <body className="bg-offwhite dark:bg-offblack transition-colors duration-500 antialiased">
+        <body>
         <ThemeProvider>
             <LanguageProvider>
-                <Navbar />
-                {children}
+                <div className="bg-offwhite dark:bg-offblack transition-colors duration-500 antialiased">
+                    <Navbar />
+                    {children}
+                </div>
             </LanguageProvider>
         </ThemeProvider>
         </body>
