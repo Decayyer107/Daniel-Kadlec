4e5d316fddcc2f5c5ee7263ac7b3d380ac69f744
Decayyer <daniel-kadlec@seznam.cz>
2025-10-05 15:33:21 +0200

    fix: about HopperElement background color

--- src/app/sections/about.tsx
+++ src/app/sections/about.tsx
@@ -17,7 +17,7 @@ export default function About(){
                 </div>
                 <div>
                     <div className={'mb-6 scale-x-[-1]'}>
-                        <HopperElement/>
+                        <HopperElement className={'!bg-gray-200 dark:!bg-gray-900 !shadow-[inset_4px_4px_6px_rgba(210,210,221,0.6)] dark:shadow-![rgba(7,7,8,0.8)]'}/>
                     </div>
                     <Button className="w-full">
                         {dict.about.button}
@@ -26,7 +26,7 @@ export default function About(){
                 </div>
             </div>
             <div className={'w-1/2'}>
-                <p className={'text-secondary text-xl'}>{dict.about.paragraph()}</p>
+                <p className={'text-secondary text-2xl'}>{dict.about.paragraph()}</p>
             </div>
         </section>
     );
--- src/components/HopperElement.tsx
+++ src/components/HopperElement.tsx
@@ -1,9 +1,13 @@
-export default function HopperElement(){
+
+interface HopperElementProps {
+    className?: string;
+}
+export default function HopperElement({className}: HopperElementProps) {
     return (
       <div className={'flex flex-col gap-1'}>
-          <div className={'hopper-element w-[50px] h-[50px]'}></div>
-          <div className={'hopper-element w-[110px] h-[50px]'}></div>
-          <div className={'hopper-element w-[170px] h-[50px]'}></div>
+          <div className={`hopper-element w-[50px] h-[50px] ${className}`}></div>
+          <div className={`hopper-element w-[110px] h-[50px] ${className}`}></div>
+          <div className={`hopper-element w-[170px] h-[50px] ${className}`}></div>
       </div>
     );
 }
