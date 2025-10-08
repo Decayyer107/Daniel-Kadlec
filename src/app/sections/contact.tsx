f50c53a56edd041cf51595e87f49e5f84b4cc3be
Decayyer <daniel-kadlec@seznam.cz>
2025-10-08 21:26:18 +0200

    fix: submit button translation

--- src/app/sections/contact.tsx
+++ src/app/sections/contact.tsx
@@ -69,6 +69,7 @@ export function About() {
 
                     <input
                         type="submit"
+                        value={dict.contact.contact_form_button}
                         className="inline-flex justify-center items-center gap-2 py-[14px] px-14 border-2 rounded-[14px] font-secondary font-bold text-xl text-offblack dark:text-offwhite border-offblack dark:border-offwhite cursor-pointer"
                     />
                 </form>
