1e612953e0ec1ad6ac99e8ac16fc9bc4b065d103
Decayyer <daniel-kadlec@seznam.cz>
2025-10-13 16:45:20 +0200

    fix: footer disappearing links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -48,6 +48,7 @@ export default function Footer() {
             document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' });
         }
     }, [pathname]);
+
     return (
         <footer className="section ">
             <div className={'bg-gray-200 dark:bg-[#0C0C0E] border-2 shadow-xl border-gray-100 dark:border-gray-900 w-full py-[clamp(_24px,_1.5vw,_36px)] rounded-[16px] sm:rounded-[24px] mb-8 px-[clamp(_24px,_1.5vw,_48px)] transition-all duration-500 flex justify-between items-center'}>
@@ -69,7 +70,7 @@ export default function Footer() {
                         <span className="text font-primary font-bold text-[clamp(_20px,_3vw,_30px)]">Daniel <span className="text-green">Kadlec</span></span>
                     </div>
                     <div>
-                        <ul className="gap-12 font-secondary font-bold text-md hidden md:flex">
+                        <ul className="flex gap-[clamp(_20px,_3vw,_42px)] font-secondary font-bold text-[clamp(_14px,_2.5vw,_18px)]">
                             <li><Link className={'text'} href="/" onClick={(e) => goHome(undefined, e)}>{dict.nav.Home}</Link></li>
                             <li><Link className={'text'} href="/#about" onClick={(e) => goHome('about', e)}>{dict.nav.About}</Link></li>
                             <li><Link className={'text'} href="/portfolio" onClick={(e) => goPortfolio(e)}>{dict.nav.Portfolio}</Link></li>
