68ed1d178168b6d38b0306aa05a9f42edf0a529a
Decayyer <daniel-kadlec@seznam.cz>
2025-10-31 12:17:29 +0100

    feat: smooth scrolling

---
+++ src/hooks/useScroll.ts
@@ -0,0 +1,25 @@
+"use client";
+import { useEffect } from "react";
+import Lenis from "@studio-freight/lenis";
+
+export default function useLenisScroll() {
+    useEffect(() => {
+        const lenis = new Lenis({
+            wrapper: document.documentElement,
+            content: document.body,
+            smoothWheel: true,
+            // @ts-expect-error smoothTouch exists at runtime
+            smoothTouch: false,
+            duration: 0.8,
+            wheelMultiplier: 1.1,
+        });
+
+        function raf(time: number) {
+            lenis.raf(time);
+            requestAnimationFrame(raf);
+        }
+        requestAnimationFrame(raf);
+
+        return () => lenis.destroy();
+    }, []);
+}
